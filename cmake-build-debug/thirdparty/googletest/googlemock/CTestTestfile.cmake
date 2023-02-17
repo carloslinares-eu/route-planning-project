b7e7a457f3a140925847bd1f641179112bc54a12
carloslinares-eu <contact@carloslinares.eu>
2023-02-17 20:29:35 +0100

    Compiling for Fedora

---
+++ cmake-build-debug/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/carloslinares-eu/Dev/route-planning-project/thirdparty/googletest/googlemock
+# Build directory: /home/carloslinares-eu/Dev/route-planning-project/cmake-build-debug/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")

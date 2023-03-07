8a88084760d3a3dd3b82aafc0e0c2df092e4e819
Carlos Linares <contact@carloslinares.eu>
2023-03-07 07:30:03 +0100

    Working copy of OSM_A_star_search

--- src/model.h
+++ src/model.h
@@ -5,63 +5,78 @@
 #include <string>
 #include <cstddef>
 
-class Model
-{
+class Model {
 public:
     struct Node {
         double x = 0.f;
         double y = 0.f;
     };
-    
+
     struct Way {
         std::vector<int> nodes;
     };
-    
+
     struct Road {
-        enum Type { Invalid, Unclassified, Service, Residential,
-            Tertiary, Secondary, Primary, Trunk, Motorway, Footway };
+        enum Type {
+            Invalid, Unclassified, Service, Residential,
+            Tertiary, Secondary, Primary, Trunk, Motorway, Footway
+        };
         int way;
         Type type;
     };
-    
+
     struct Railway {
         int way;
-    };    
-    
+    };
+
     struct Multipolygon {
         std::vector<int> outer;
         std::vector<int> inner;
     };
-    
-    struct Building : Multipolygon {};
-    
-    struct Leisure : Multipolygon {};
-    
-    struct Water : Multipolygon {};
-    
+
+    struct Building : Multipolygon {
+    };
+
+    struct Leisure : Multipolygon {
+    };
+
+    struct Water : Multipolygon {
+    };
+
     struct Landuse : Multipolygon {
-        enum Type { Invalid, Commercial, Construction, Grass, Forest, Industrial, Railway, Residential };
+        enum Type {
+            Invalid, Commercial, Construction, Grass, Forest, Industrial, Railway, Residential
+        };
         Type type;
     };
-    
-    Model( const std::vector<std::byte> &xml );
-    
-    auto MetricScale() const noexcept { return m_MetricScale; }    
-    
+
+    Model(const std::vector<std::byte> &xml);
+
+    auto MetricScale() const noexcept { return m_MetricScale; }
+
     auto &Nodes() const noexcept { return m_Nodes; }
+
     auto &Ways() const noexcept { return m_Ways; }
+
     auto &Roads() const noexcept { return m_Roads; }
+
     auto &Buildings() const noexcept { return m_Buildings; }
+
     auto &Leisures() const noexcept { return m_Leisures; }
+
     auto &Waters() const noexcept { return m_Waters; }
+
     auto &Landuses() const noexcept { return m_Landuses; }
+
     auto &Railways() const noexcept { return m_Railways; }
-    
+
 private:
     void AdjustCoordinates();
-    void BuildRings( Multipolygon &mp );
+
+    void BuildRings(Multipolygon &mp);
+
     void LoadData(const std::vector<std::byte> &xml);
-    
+
     std::vector<Node> m_Nodes;
     std::vector<Way> m_Ways;
     std::vector<Road> m_Roads;
@@ -70,7 +85,7 @@ private:
     std::vector<Leisure> m_Leisures;
     std::vector<Water> m_Waters;
     std::vector<Landuse> m_Landuses;
-    
+
     double m_MinLat = 0.;
     double m_MaxLat = 0.;
     double m_MinLon = 0.;

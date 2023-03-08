e8c1aaa5bcac87e767edbd308a24c4ec6f0ac6cf
carloslinares-eu <contact@carloslinares.eu>
2023-03-08 07:38:54 +0100

    Cleaning build and debug folders

--- cmake-build-debug/thirdparty/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carloslinares-eu/Dev/route-planning-project/thirdparty/googletest
-# Build directory: /home/carloslinares-eu/Dev/route-planning-project/cmake-build-debug/thirdparty/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
